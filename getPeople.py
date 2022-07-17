414a7aea45324a4abbb4286e95bcd9713d654c78
Francesca La Manna <57345846+LamFra@users.noreply.github.com>
2022-07-17 20:53:13 +0200

    Update getPeople.py

--- getPeople.py
+++ getPeople.py
@@ -6,12 +6,13 @@ dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:4566")
 
 table_name = "CentroCommercialeCampania"
 
-print("Welcome into the CountingPeople application.")
+
+print("**** Welcome into the CountingPeople application! ****")
 print("Enter the name of one or more shops you want to know the number of people within it")
 shops = input("(shops must be separated by one space):\n")
 shops = shops.split()
 table = dynamodb.Table(table_name)
-print("------------------------------------------------------------------------------------------------")
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
 for shop in shops:
 	try:
 		response = table.get_item(Key={'shop': shop})
@@ -22,4 +23,4 @@ for shop in shops:
 		% (response['Item']['shop'], response['Item']['people']))
 	print("-measured at %s" 
 		% (response['Item']['measure_date']))
-	print("------------------------------------------------------------------------------------------------")
+	print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
