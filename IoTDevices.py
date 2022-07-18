1c415eb47b24f17b8369fec7f6768b27cc90dab5
Francesca La Manna <57345846+LamFra@users.noreply.github.com>
2022-07-18 18:30:56 +0200

    Update IoTDevices.py

--- IoTDevices.py
+++ IoTDevices.py
@@ -6,6 +6,8 @@ sqs = boto3.resource('sqs', endpoint_url='http://localhost:4566')
 
 shops = [('Carpisa', 5), ('Adidas', 8), ('Carrefour', 6), ('Sephora', 7), ('Zuiki', 8)]
 
+q = sqs.get_queue_by_name(QueueName="summary")
+
 for shop, max_capacity in shops:
 	queue = sqs.get_queue_by_name(QueueName=shop)
 	measure_date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
@@ -19,3 +21,6 @@ for shop, max_capacity in shops:
 	msg_body2 = '{"shop": "%s","measure_date": "%s","people": "%s","sensor_type": "%s"}' % (shop, measure_date, str(people2), "out")
 	print(msg_body2)
 	queue.send_message(MessageBody=msg_body2)
+	
+	msg_body3 = '{"shop": "%s","measure_date": "%s","people1": "%s","people2": "%s"}' % (shop, measure_date, str(people1), str(people2))
+	q.send_message(MessageBody=msg_body3)
