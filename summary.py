e71be07cdfea57683f2a68ecb29351b2f3a137ed
Francesca La Manna <57345846+LamFra@users.noreply.github.com>
2022-07-19 18:38:01 +0200

    Update summary.py

--- summary.py
+++ summary.py
@@ -8,13 +8,14 @@ def lambda_handler(event, context):
 	key = "<key>"
 	url = "https://maker.ifttt.com/trigger/summary/with/key/"+key
 	
-	minutes = int('{:02d}'.format(datetime.datetime.now().minute()))
+	time = 2
+	minutes = int('{:02d}'.format(datetime.datetime.now().minute))
 	
-	if minutes == 0:
+	if (minutes % time) == 0:
 		people = 0
 		for record in event['Records']:
 			payload = record['body']
 			payload = json.loads(str(payload))
 			people += int(payload['people1']) - int(payload['people2'])
-		average = int(people / 60) 
-		req = requests.post(url, json={"value1": average})
+		average = int(people / time)
+		req = requests.post(url, json={"value1": str(average)})
